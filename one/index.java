f514657eb19d07911444909a7b935dd7f1115476
Jatin Sharma <jatina9410@gmail.com>
2025-12-26 22:11:05 +0530

    2nd one

--- one/index.java
+++ one/index.java
@@ -1,5 +1,10 @@
 class index{
     public static void main(String args[]){
         System.out.println("hey");
+
+        String name = "jatin";
+        int age = 90;
+
+        System.out.println(age);
     }
 }
