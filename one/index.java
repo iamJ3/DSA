1e93b05a3c6f17acd87bd80f0e059e7f4d184e7d
Jatin Sharma <jatina9410@gmail.com>
2025-12-27 13:10:29 +0530

    added canner callss

--- one/index.java
+++ one/index.java
@@ -1,9 +1,14 @@
+// import java.utill.Scanner;
+import java.util.Scanner;
 class index{
     public static void main(String args[]){
         System.out.println("hey");
 
+        Scanner sc = new Scanner(System.in);
+        int age = sc.nextInt();
+
         String name = "jatin";
-        int age = 90;
+        int umar = 90;
 
         System.out.println(age);
     }
