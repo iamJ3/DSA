6315ee2d8676d9bd571adb02a4cacc25c410a72e
Jatin Sharma <jatina9410@gmail.com>
2025-12-30 16:24:54 +0530

    4th lecture

---
+++ four/main.java
@@ -0,0 +1,11 @@
+
+public class main {
+
+    public static void main(String args[]) {
+
+        for (int count = 1; count <= 10; count++) {
+
+            System.out.println(count);
+        }
+    }
+}
