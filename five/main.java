cb326b6fadadbf79cc6b97a73f3d957d4030c416
Jatin Sharma <jatina9410@gmail.com>
2026-01-02 20:11:16 +0530

    java pattern two

--- five/main.java
+++ five/main.java
@@ -1,7 +1,13 @@
-package five;
-
 public class main {
+
     public static void main(String[] args) {
+        int n = 5, m = 4;
         
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <=n; j++) {
+                System.out.print('*');
+            }
+            System.out.println(' ');
+        }
     }
 }
